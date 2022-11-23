d1e471efdb121a4b635ba08d19c919c3c665b9d6
unknown <yzhaoinuw@gmail.com>
2022-11-23 11:07:05 -0500

    added lines in run_experiments.py that select subsets of columns model comparison.

--- code/run_experiments.py
+++ code/run_experiments.py
@@ -60,73 +60,66 @@ def run_experiment(
 
 
 #%%
-DATA_PATH = "../data/"
+if __name__ == "__main__":
+    DATA_PATH = "../data/"
 
-# previous data
-# feature_file = "Tables_1_2_data.xlsx"
-# label_file = "label.xlsx"
-# df_features = pd.read_excel(DATA_PATH + feature_file)
-# df_labels = pd.read_excel(DATA_PATH + label_file)
+    # previous data
+    # feature_file = "Tables_1_2_data.xlsx"
+    # label_file = "label.xlsx"
+    # df_features = pd.read_excel(DATA_PATH + feature_file)
+    # df_labels = pd.read_excel(DATA_PATH + label_file)
 
-# new data
-feature_file = "features_processed.xlsx"
-df_features = pd.read_excel(DATA_PATH + feature_file)
-df_labels = df_features["LP"].to_frame()
-df_features = df_features.drop("LP", axis=1)
+    # new data
+    feature_file = "features_processed.xlsx"
+    df_features = pd.read_excel(DATA_PATH + feature_file)
+    df_labels = df_features["LP"].to_frame()
+    df_features = df_features.drop("LP", axis=1)
 
-# try some transformation
-df_labels = (df_labels - 4) * 1
+    # drop features
+    df_features = df_features.drop("IOLPowerInsertedD", axis=1)
 
-# dropping columns
-# df_features = df_features.drop("Age", axis=1)
-# df_features = df_features[["EPP/LT", "ACD_pre (mm)"]]
-runs = 1000
-EPOCHS = 100
-hidden_size = 16
-batch_size = 8
-normalization = True
-losses = run_experiment(
-    df_features,
-    df_labels,
-    runs=runs,
-    h1=hidden_size,
-    epochs=EPOCHS,
-    normalization=normalization,
-    batch_size=batch_size,
-)
+    # select subset of features
+    use_features = [
+        "AxialLengthmm",
+        "RAC",
+        "Sex_1",
+        "Sex_2",
+        "IOLModel_1",
+        "IOLModel_2",
+        "IOLModel_3",
+    ]
 
-#%%
+    # Edu's benchmark
+    # use_features = ["RAC", "AxialLengthmm", "IOLModel_1", "IOLModel_2", "IOLModel_3"]
+    df_features = df_features[use_features]
 
-test_losses = losses["test_losses"]
-baseline_losses = losses["baseline_losses"]
+    # try some transformation
+    df_labels = (df_labels - 4) * 1
 
-print(f"hidden_size: {hidden_size}")
-print(f"epochs: {EPOCHS}")
-print(f"test loss mean: {test_losses.mean()}")
-print(f"test loss std: {test_losses.std()}")
-"""
-train_losses = losses["train_losses"]
-test_losses = losses["test_losses"]
+    # dropping columns
+    # df_features = df_features.drop("Age", axis=1)
+    # df_features = df_features[["EPP/LT", "ACD_pre (mm)"]]
+    runs = 1000
+    EPOCHS = 50
+    hidden_size = 32
+    batch_size = 8
+    normalization = True
+    losses = run_experiment(
+        df_features,
+        df_labels,
+        runs=runs,
+        h1=hidden_size,
+        epochs=EPOCHS,
+        normalization=normalization,
+        batch_size=batch_size,
+    )
 
-skip_epochs = 50
-plt.title("Training and Test Loss")
-plt.plot(
-    list(range(skip_epochs + 1, EPOCHS + 1)), test_losses[skip_epochs:], label="test"
-)
-plt.plot(
-    list(range(skip_epochs + 1, EPOCHS + 1)), train_losses[skip_epochs:], label="train"
-)
-plt.xlabel("iterations")
-plt.ylabel("Loss")
-#plt.ylim((0, 1))
-plt.grid()
-plt.legend()
-plt.show()
+    #%%
 
-#%%
-plt.figure(figsize=(10, 5))
-plt.bar([str(k) for k in range(1, len(df_labels)+1)], sample_loss/runs)
-plt.title(f"Average MAE per Sample (from {runs} runs)")
-plt.xlabel("Sample ID")
-plt.ylabel("Average Loss")
-"""
+    test_losses = losses["test_losses"]
+    baseline_losses = losses["baseline_losses"]
+
+    print(f"hidden_size: {hidden_size}")
+    print(f"epochs: {EPOCHS}")
+    print(f"test loss mean: {test_losses.mean()}")
+    print(f"test loss std: {test_losses.std()}")
